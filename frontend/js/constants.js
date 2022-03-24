6e257abcb9976905a52ff1f31960e10f3f16e9ae
Tahee_YT <dragontahahafeez@gmail.com>
2022-03-24 18:21:17 -0400

    rinky test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x60E650ac97F6373F5a9f1c56E322DDBB53E02888"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
